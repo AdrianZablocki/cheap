b7a65386c405b0a75d314e111db7f9e422448d1d
Adrian Zabłocki <adrian.zablocki@polskieradio.pl>
2023-12-04 21:55:06 +0100

    Changed model schema

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,21 +1,32 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const postSchema = new mongoose.Schema({
   author: {
-    type: String,
-    required: [true, 'Please add author']
+    type: String
   },
   date: {
     type: Date
   },
   name: {
     type: String,
-    required: [true, 'Please add title']
+    required: [true, 'Nazwa suszu jest polem wymaganym']
+  },
+  region: {
+    type: String,
+    required: [true, 'Województwo jest polem wymaganym']
+  },
+  citi: {
+    type: String,
+    required: [true, 'Miasto jest polem wymaganym']
+  },
+  adress: {
+    type: String,
+    required: [true, 'Adres apteki jest polem wymaganym']
   },
   price: {
     type: Number,
-    required: [true, 'Please add price']
+    required: [true, 'Cena jest polem wymaganym']
   }
-});
+})
 
-export default mongoose.models.Post || mongoose.model('Post', postSchema);
+export default mongoose.models.Post || mongoose.model('Post', postSchema)
