b025885dcd8f27c4531d8082f47aea5e52df9e92
Adrian Zabłocki <adrian.zablocki@polskieradio.pl>
2024-01-17 22:34:29 +0100

    Remove vscode dir

--- app/verified/page.js
+++ app/verified/page.js
@@ -11,22 +11,22 @@ const setValidationFlag = async (params) => {
 
 const VerifiedPage = async({ searchParams }) => {
 
+  console.log('PARAMS', searchParams)
   const verified = await setValidationFlag(searchParams)
 
-  // const responseMap = new Map([
-  //   [ 'Link aktywacyjny wygasł', () => <Link href="/activation-link">Link aktywacyjny wygasł</Link> ],
-  //   [ 'Konto zostało zweryfikowne poprawnie', () => redirect('/login') ],
-  //   [ 
-  //     'Coś z tym linkiem jest nie tak',
-  //     () => <div key="failed">Coś poszło nie tak, prosimy o <a href="mailto:kontakt@chaepweed.pl">kontakt</a></div>
-  //   ],
-  //   [ 'default', () => <div key="total failed">Coś totalnie poszło nie tak, najlepiej skontaktuj się z nami</div> ]
-  // ])
+  const responseMap = new Map([
+    [ 'Link aktywacyjny wygasł', () => <Link href="/activation-link">Link aktywacyjny wygasł</Link> ],
+    [ 'Konto zostało zweryfikowne poprawnie', () => redirect('/login') ],
+    [ 
+      'Coś z tym linkiem jest nie tak',
+      () => <div key="failed">Coś poszło nie tak, prosimy o <a href="mailto:kontakt@chaepweed.pl">kontakt</a></div>
+    ],
+    [ 'default', () => <div key="total failed">Coś totalnie poszło nie tak, najlepiej skontaktuj się z nami</div> ]
+  ])
   
   return (
     <div>
-      {/* {verified && responseMap.get(verified.message || 'default')()} */}
-      <div>huj</div>
+      {verified && responseMap.get(verified.message || 'default')()}
     </div>
   )
 }
